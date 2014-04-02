a059b65f6e8fe7bdbd07a4ec54e175a88e3527f0
Peter Anderson <anderson.peter@byu.edu>
2014-04-02 13:55:14 -0600

    Finished adding web forms to the app and started configuring for database

--- flask/microblog/app/__init__.py
+++ flask/microblog/app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLALCHEMY(app)
 
-
-from app import views
+from app import views, models
